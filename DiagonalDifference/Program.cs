8dde47a32e9196447124b1bcf4a0a855f9ad7594
Atikul1213 <atikul.islam@brainstation-23.com>
2025-09-11 19:54:10 +0600

    solve DiagonalDifference

---
+++ DiagonalDifference/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+{
+    // link  https://www.hackerrank.com/challenges/diagonal-difference/submissions/code/447087732
+    public static void Main(string[] args)
+    {
+        List<List<int>> arr = new List<List<int>>();
+
+        int len = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < len; i++)
+        {
+            var input = Array.ConvertAll(Console.ReadLine().Split(' '), Convert.ToInt32);
+            arr.Add(input.ToList());
+        }
+
+        int sum1 = 0, sum2 = 0;
+        for (int i = 0; i < len; i++)
+        {
+            sum1 += arr[i][i];
+            sum2 += arr[i][len - i - 1];
+        }
+
+        Console.WriteLine(Math.Abs(sum1 - sum2));
+    }
+}
