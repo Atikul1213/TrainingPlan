4b2e7dffef6e56cfed1260b2b74b50127627d271
Atikul1213 <atikul.islam@brainstation-23.com>
2025-09-11 19:49:28 +0600

    array manipulation problem solve

---
+++ ArrayManipulation/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Program
+{
+    // link https://www.hackerrank.com/challenges/sparse-arrays/submissions/code/446950871
+    public static void Main(string[] args)
+    {
+        var n = Convert.ToInt32(Console.ReadLine().Trim());
+        var dict = new Dictionary<string, int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            var value = Console.ReadLine();
+            if (dict.ContainsKey(value))
+            {
+                dict[value]++;
+            }
+            else
+            {
+                dict[value] = 1;
+            }
+        }
+
+        var query = Convert.ToInt32(Console.ReadLine());
+
+        while (query > 0)
+        {
+            var key = Console.ReadLine();
+            if (dict.ContainsKey(key))
+                Console.WriteLine(dict[key]);
+            else
+                Console.WriteLine(0);
+            query--;
+        }
+    }
+}
