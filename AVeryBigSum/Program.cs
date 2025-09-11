db5c67b53ae00ee8523852bc2e1c4f014f681966
Atikul1213 <atikul.islam@brainstation-23.com>
2025-09-11 19:45:37 +0600

    solve a very big sum problem

---
+++ AVeryBigSum/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+
+    }
+
+    public static long aVeryBigSum(List<long> ar)
+    {
+        long sum = 0;
+        foreach (var item in ar)
+            sum += item;
+
+        return sum;
+    }
+}
