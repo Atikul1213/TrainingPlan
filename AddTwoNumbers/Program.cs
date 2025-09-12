38c6be6de716c7e9c1605980be3b857739f0889e
Atikul1213 <atikul.islam@brainstation-23.com>
2025-09-12 16:19:17 +0600

    Sum two number linked list problem

---
+++ AddTwoNumbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using AddTwoNumbers;
+
+public class Program
+{
+    // link https://leetcode.com/problems/add-two-numbers/submissions/1768143445/?sorting=W3sic29ydE9yZGVyIjoiQVNDRU5ESU5HIiwib3JkZXJCeSI6IkRJRkZJQ1VMVFkifV0%3D&page=11
+    public static void Main(string[] args)
+    {
+
+    }
+
+    public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0)
+        {
+            int sum = carry;
+
+            if (l1 != null)
+            {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null)
+            {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+            int digit = sum % 10;
+
+            current.next = new ListNode(digit);
+            current = current.next;
+        }
+
+        return dummy.next;
+
+    }
+}
