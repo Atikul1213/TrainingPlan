c7ca92308d889fc41822113ee1b7610dc6d1e42e
Atikul1213 <atikul.islam@brainstation-23.com>
2025-09-12 16:02:30 +0600

    solve valid parenthesis

---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,48 @@
+﻿public class Program
+{
+    // https://leetcode.com/problems/valid-parentheses/submissions/1768129981/?sorting=W3sic29ydE9yZGVyIjoiQVNDRU5ESU5HIiwib3JkZXJCeSI6IkRJRkZJQ1VMVFkifV0%3D&page=11
+    public static void Main(string[] args)
+    {
+        var input = Console.ReadLine();
+        int len = input.Length;
+        var stack = new Stack<char>();
+        bool isValid = true;
+
+        for (int i = 0; i < len; i++)
+        {
+            char ch = input[i];
+            if (ch == '(' || ch == '{' || ch == '[')
+            {
+                stack.Push(ch);
+            }
+            else
+            {
+                if (!stack.Any())
+                {
+                    isValid = false;
+                    break;
+                }
+                else
+                {
+                    var top = stack.Peek();
+                    if (ch == ')' && top == '(')
+                        stack.Pop();
+                    else if (ch == '}' && top == '{')
+                        stack.Pop();
+                    else if (ch == ']' && top == '[')
+                        stack.Pop();
+                    else
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (stack.Any())
+            isValid = false;
+
+        Console.WriteLine("isValid: ", isValid);
+    }
+}
