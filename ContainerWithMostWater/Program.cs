afea376a36b5daca2a78f49f3e020381d948e911
Atikul1213 <atikul.islam@brainstation-23.com>
2025-09-16 18:09:18 +0600

    solve container with most water

---
+++ ContainerWithMostWater/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Program
+{
+    // link   https://leetcode.com/problems/container-with-most-water/submissions/1772687698
+    public static void Main(string[] args)
+    {
+        List<List<int>> arr = new List<List<int>>();
+
+        // int len = Convert.ToInt32(Console.ReadLine());
+        var height = Array.ConvertAll(Console.ReadLine().Split(' '), Convert.ToInt32);
+
+        int st = 0, ed = height.Length - 1, max = 0, value = 0;
+        while (st < ed)
+        {
+            value = Math.Min(height[st], height[ed]) * (ed - st);
+            max = Math.Max(max, value);
+
+            if (height[st] <= height[ed])
+            {
+                st++;
+            }
+            else ed--;
+        }
+
+        Console.WriteLine("Max: " + max);
+
+    }
+}
