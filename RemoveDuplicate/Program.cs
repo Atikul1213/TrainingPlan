a4b47f87a3ed0c7bfb5e91cb76cdfec5742d467f
Atikul1213 <atikul.islam@brainstation-23.com>
2025-09-16 17:28:18 +0600

    solve remove duplicate

---
+++ RemoveDuplicate/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Program
+{
+    // link https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/1772656367
+    public static void Main()
+    {
+        List<List<int>> arr = new List<List<int>>();
+
+        int len = Convert.ToInt32(Console.ReadLine());
+        var nums = Array.ConvertAll(Console.ReadLine().Split(' '), Convert.ToInt32);
+
+        var hset = new HashSet<int>();
+        int uniqueCount = 0;
+
+        foreach (var num in nums)
+        {
+            if (!hset.Contains(num))
+            {
+                hset.Add(num);
+            }
+        }
+        int idx = 0;
+        foreach (var item in hset)
+        {
+            nums[idx++] = item;
+        }
+
+        foreach (var item in hset)
+        {
+            Console.WriteLine("item: " + item);
+        }
+
+        Console.WriteLine("Length: " + hset.Count());
+    }
+}
