ed250ce913cd9500ab34a171fa92d31aff6f405c
Atikul1213 <atikul.islam@brainstation-23.com>
2025-09-25 15:04:48 +0600

    solve array of prefix xor

---
+++ ArrayOfPrefixXor/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        FindArray(new int[] { 5, 2, 0, 3, 1 });
+        Console.WriteLine("Hello word");
+    }
+
+
+
+    public static int[] FindArray(int[] pref)
+    {
+        var ans = new int[pref.Length];
+        ans[0] = pref[0];
+
+        for (int i = 1; i < pref.Length; i++)
+        {
+            ans[i] = pref[i - 1] ^ pref[i];
+        }
+
+        for (int i = 0; i < pref.Length; i++)
+        {
+            Console.WriteLine("i: " + i + "  value: " + ans[i]);
+        }
+        return ans;
+    }
+
+
+}
