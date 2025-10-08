869cc89e6978c19689d9b39d61aba978c5f7afad
Atikul1213 <atikul.islam@brainstation-23.com>
2025-10-08 14:23:05 +0600

    solve SingleNumber problem

---
+++ SingleNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    // link  https://leetcode.com/problems/single-number/submissions/1795092731
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Hello word");
+    }
+
+    public static int SingleNumber(int[] nums)
+    {
+        int ans = 0;
+        foreach (var num in nums)
+        {
+            ans = ans ^ num;
+        }
+
+
+        return ans;
+    }
+}
