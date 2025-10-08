46b13de73507b2c90d8376dcc4191678b9c391e5
Atikul1213 <atikul.islam@brainstation-23.com>
2025-10-08 15:01:50 +0600

    solve second highest salary

---
+++ SecondHighestSalary/Program.cs
@@ -0,0 +1,12 @@
+﻿
+//link  https://leetcode.com/problems/second-highest-salary/submissions/1795118857
+
+/*
+# Write your MySQL query statement below
+select max(salary) as SecondHighestSalary
+from Employee
+where salary<>(select max(salary)
+    from Employee
+);
+*/
+Console.WriteLine("Hello, World!");
