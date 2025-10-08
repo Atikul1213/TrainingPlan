5e28b3597fb5beb9ed7917ba49542e22a3cce079
Atikul1213 <atikul.islam@brainstation-23.com>
2025-10-08 14:53:00 +0600

    solve combine two table

---
+++ CombineTwoTable/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    // link  https://leetcode.com/problems/combine-two-tables/submissions/1795114145
+    public static void Main(string[] args)
+    {
+        //select Person.firstName, Person.lastName, Address.city, Address.state
+        //from Person left join Address
+        //on Person.personId = Address.personId;
+
+
+
+        Console.WriteLine("Hello word");
+    }
+}
