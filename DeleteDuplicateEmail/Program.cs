75a9b90660941f4cd31a0d818641c27a33f1b7fe
Atikul1213 <atikul.islam@brainstation-23.com>
2025-10-13 14:02:51 +0600

    solve delete duplicate  email

---
+++ DeleteDuplicateEmail/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    // link  https://leetcode.com/problems/delete-duplicate-emails/submissions/1800102025
+    public static void Main(string[] args)
+    {
+        /* 
+         # Write your MySQL query statement below
+            delete p1
+            from Person as p1, Person as p2
+            where p1.email = p2.email AND p1.id > p2.id;
+         */
+        Console.WriteLine("Hello word");
+    }
+
+}
