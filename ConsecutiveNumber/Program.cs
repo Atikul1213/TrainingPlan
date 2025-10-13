9846af526e29b22399a361eab0a92c9c41895ca0
Atikul1213 <atikul.islam@brainstation-23.com>
2025-10-13 15:13:26 +0600

    solve consecutive numbers

---
+++ ConsecutiveNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    // Link: https://leetcode.com/problems/consecutive-numbers/submissions/1800147435
+    public static void Main(string[] args)
+    {
+        /*
+        # Write your MySQL query statement below
+            select distinct(l1.num) as ConsecutiveNums 
+            from Logs as l1, Logs as l2, Logs as l3
+            where l2.id = l1.id-1 AND l3.id = l2.id-1
+                AND l1.num = l2.num AND l2.num = l3.num;
+         */
+        Console.WriteLine("Hello, World!");
+    }
+}
