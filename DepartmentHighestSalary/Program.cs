f14ea1f5794c71ec24ddc3e4547143045fd345ba
Atikul1213 <atikul.islam@brainstation-23.com>
2025-10-13 15:54:16 +0600

    solve department highest salary

---
+++ DepartmentHighestSalary/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    // Link: https://leetcode.com/problems/department-highest-salary/submissions/1800177320
+    public static void Main(string[] args)
+    {
+        /*
+        # Write your MySQL query statement below
+        select d.name as Department, e.name as Employee, e.salary as Salary
+        from Employee  as e
+        join Department as d
+        on e.departmentId = d.id
+        where e.salary = (select max(salary)
+                    from Employee
+                    where departmentId = e.departmentId
+                );
+ 
+         */
+        Console.WriteLine("Hello, World!");
+    }
+}
