d7a3ea56e1c07ee14bdca29a6ff87aeed8c36d4b
Atikul1213 <atikul.islam@brainstation-23.com>
2025-10-13 12:48:20 +0600

    solve rank score

---
+++ RankScore/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program
+{
+    //link https://leetcode.com/problems/rank-scores/submissions/1800050195
+    public static void Main(string[] args)
+    {
+        /*  
+         Select s1.score, count(s2.score) as 'rank'
+        From Scores as s1,
+        (select distinct score from Scores) s2
+        where s1.score <= s2.score
+        group by s1.id
+        order by s1.score desc;
+         */
+        Console.WriteLine("Hello word");
+    }
+}
